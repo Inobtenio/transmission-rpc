7fbd2372ba8952c46665f0c3891b5891d1d556c0
Kevin Martin <knmartinm@gmail.com>
2020-06-21 03:35:30 -0500

    Add packages

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018 Trim21 <trim21me@hotmail.com>
 # Licensed under the MIT license.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
         name="transmissionrpc",
         version="3.0.1",
+        packages=find_packages(),
     )
